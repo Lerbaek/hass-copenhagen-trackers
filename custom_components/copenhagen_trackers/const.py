a82f90921fa61fc932e08e2cd66f9f5c3f662f14
Kristoffer Lerbæk <github@lerbaek.dk>
2024-12-28 12:13:39 +0100

    Bump hassfest action to v4

--- custom_components/copenhagen_trackers/const.py
+++ custom_components/copenhagen_trackers/const.py
@@ -10,4 +10,5 @@ CONF_ACCESS_TOKEN = "access_token"
 DEFAULT_SCAN_INTERVAL = timedelta(hours=1)
 
 ATTR_DESCRIPTION = "description"
+ATTR_LOCATION = "location"
 ATTR_UPDATED_AT = "updated_at"
--- custom_components/copenhagen_trackers/device_tracker.py
+++ custom_components/copenhagen_trackers/device_tracker.py
@@ -1,7 +1,10 @@
 """Device tracker platform for Copenhagen Trackers integration."""
 from homeassistant.components.device_tracker import SourceType, TrackerEntity
 from homeassistant.const import Platform
-from .const import DOMAIN
+from .const import (
+    DOMAIN,
+    ATTR_LOCATION as LOCATION,
+)
 from . import CopenhagenTrackersEntity
 
 async def async_setup_entry(hass, entry, async_add_entities):
@@ -20,32 +23,41 @@ class DeviceTracker(CopenhagenTrackersEntity, TrackerEntity):
     _attr_icon = "mdi:map-marker"
     _attr_entity_category = None
     PLATFORM = Platform.DEVICE_TRACKER
-    SUFFIX = "location"
+    SUFFIX = LOCATION
 
     @property
     def source_type(self):
         """Return the source type of the device."""
         return SourceType.GPS
+    
+    def get_location_details(self, key: str) -> float:
+        """Get a location detail"""
+        return float(self.get_location.get("details", {}).get(key, 0))
+    
+    @property
+    def location(self) -> dict:
+        """Get a location detail"""
+        return self.device_data.get(LOCATION, {})
 
     @property
     def latitude(self):
         """Return latitude value of the device."""
-        return float(self.device_data.get("location", {}).get("details", {}).get("lat", 0))
+        return self.get_location_details("lat", 0)
 
     @property
     def longitude(self):
         """Return longitude value of the device."""
-        return float(self.device_data.get("location", {}).get("details", {}).get("lon", 0))
+        return self.get_location_details("lon", 0)
 
     @property
     def location_accuracy(self):
         """Return the location accuracy of the device."""
-        return float(self.device_data.get("location", {}).get("details", {}).get("acc", 0))
+        return self.get_location_details("acc", 0)
 
     @property
     def location_name(self):
         """Return the location name."""
-        location = self.device_data.get("location", {})
+        location = self.device_data.get(LOCATION, {})
         if road := location.get("road"):
             return f"{road}, {location.get('city', '')}, {location.get('country', '')}"
         return None
